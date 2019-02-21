c8ea3b27fe8e3b7823c9f082de2f8c40db5d4f16
Rajat Sharma <rajatsharma369007@gmail.com>
2019-02-21 12:21:14 +0530

    defined discriminant function
    there is still some error is matrix shape

--- decision.py
+++ decision.py
@@ -1,33 +1,37 @@
 import pandas as pd
 import numpy as np
+import math
 
 df = pd.read_excel("data.xlsx")
 
 print(df)
 
-# input vector
-x1 = np.array(df['w1'])
-x2 = np.array(df['w2'])
-x1 = x1.reshape(2,4)
-x2 = x2.reshape(2,4)
+# extracting inputs from file
+x1w1 = np.array(df['x1w1'])
+x2w1 = np.array(df['x2w1'])
+x1w2 = np.array(df['x1w2'])
+x2w2 = np.array(df['x2w2'])
 
-print("X1 = ", x1)
-print("X2 = ", x2)
+xvec1 = np.stack((x1w1,x2w1), axis=1)
+xvec2 = np.stack((x1w2,x2w2), axis=1)
 
 # mean vector
-mean1 = np.mean(x1, axis=1)
-mean2 = np.mean(x2, axis=1)
+mean1 = np.mean(xvec1, axis=0)
+mean2 = np.mean(xvec2, axis=0)
 
-print("mean 1 = ",mean1)
-print("mean 2 = ",mean2)
+# creating the covariance matrix
+cov1 = np.cov(xvec1)
+cov2 = np.cov(xvec2)
 
-cov1 = np.cov(x1)
-cov2 = np.cov(x2)
+# creating the inverse of the covariance matrix
+inv_cov1 = np.linalg.inv(cov1)
+inv_cov2 = np.linalg.inv(cov2)
 
-print("covariance 1 = ",cov1)
-print("covariance 2 = ",cov2)
+# prior probability
+w1 = w2 = 1/2
 
-
-
+# calculating discriminant matrix
+g1 = (-1/2)*np.matmul(np.matmul((xvec1 - mean1).T, inv_cov1), (xvec1 - mean1)) - (1/2)*(math.log(np.linalg.det(cov1))) + math.log(w1)
+g1 = (-1/2)*np.matmul(np.matmul((xvec2 - mean2).T, inv_cov2), (xvec2 - mean2)) - (1/2)*(math.log(np.linalg.det(cov2))) + math.log(w2)
 
 
