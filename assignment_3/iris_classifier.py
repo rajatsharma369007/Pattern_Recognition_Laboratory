d5cf85f2e1d8789aa27ebf333984a33382efb516
Rajat Sharma <rajatsharma369007@gmail.com>
2019-04-04 14:58:13 +0530

    modularized the iris_classifier.py

--- assignment_3/iris_classifier.py
+++ assignment_3/iris_classifier.py
@@ -1,79 +1,30 @@
 '''
 this is the fourth assignment of pattern recognition laboratory, implementing 
-backpropagation
+backpropagation on iris dataset
 '''
-
-import numpy as np
+# importing libraries
 from utils import read_dataset
+from utils import model
 
 # reading data
 X_train, X_test, y_train, y_test = read_dataset.read_irisdata("../dataset/Iris.csv")
 
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
+# initializing the model
 n_hidden = 2
-epochs = 20000
+epochs = 8000
 learnrate = 0.01
 
-n_records, n_features = X_train.shape
-last_loss = None
+# fitting the model
+'''
+function : train_irismodel
+arguments : X features, y label, no of epochs, learning rate, no of hidden layer
+'''
+iris_model = model.train_irismodel(X_train, y_train, epochs, learnrate, n_hidden)
 
-weights_input_hidden = np.random.normal(scale = 1 / n_features ** 0.5, size = (n_features, n_hidden))
-weights_hidden_output = np.random.normal(scale = 1 / n_features ** 0.5, size = n_hidden)
-
-for e in range(epochs):
-    # initializing all the delta weights with zeros
-    del_w_input_hidden = np.zeros(weights_input_hidden.shape)
-    del_w_hidden_output = np.zeros(weights_hidden_output.shape)
-    # In each epoch, and in each value of column
-    for x, y in zip(X_train.values, y_train):
-        # forward pass
-        hidden_input = np.dot(x, weights_input_hidden)
-        hidden_output = sigmoid(hidden_input)
-        output = sigmoid(np.dot(hidden_output, weights_hidden_output))
-        
-        # backward pass
-        error = y - output
-        # calculate the error term for the output unit,
-        output_error_term = error * output * (1 - output)
-        
-        # propagate the errors to hidden layers
-        # hidden layer's contribution to the error
-        hidden_error = np.dot(output_error_term, weights_hidden_output)
-        # error term for the hidden layer
-        hidden_error_term = hidden_error * hidden_output * (1 - hidden_output)
-        
-        # update the change in weights
-        del_w_hidden_output += output_error_term * hidden_output
-        del_w_input_hidden += hidden_error_term * x[:, None]
-        
-    weights_input_hidden += learnrate * del_w_input_hidden / n_records
-    weights_hidden_output += learnrate * del_w_hidden_output / n_records 
-    
-        # Printing out the mean square error on the training set
-    if e % (epochs / 10) == 0:
-        hidden_output = sigmoid(np.dot(x, weights_input_hidden))
-        out = sigmoid(np.dot(hidden_output, weights_hidden_output))
-        loss = np.mean((out - y_train) ** 2)
-
-        if last_loss and last_loss < loss:
-            print("Train loss: ", loss)
-        else:
-            print("Train loss: ", loss)
-        last_loss = loss
-        
-
-# Calculate accuracy on test data
-hidden = sigmoid(np.dot(X_test, weights_input_hidden))
-out = sigmoid(np.dot(hidden, weights_hidden_output))
-predictions = out > 0.5
-accuracy = np.mean(predictions == y_test)
-print("Prediction accuracy: {:.3f}".format(accuracy))
-
-
-
-
+# testing the model
+'''
+function : predict_irismodel
+arguments : X features, y label, iris_model
+'''
+predictions = model.predict_irismodel(X_test, y_test, iris_model)
 
--- assignment_3/utils/model.py
+++ assignment_3/utils/model.py
@@ -1,5 +1,5 @@
 '''
-This script is for training the model and prediction
+This script is for training and testing the model and prediction
 '''
 
 # importing libraries
@@ -17,7 +17,7 @@ def train(X, y, epoch, learning_rate):
         
         for j in range(len(y)):
             D_actual = weight[0] + weight[1]*X[j][0] + weight[2]*X[j][1]
-            signD = activation(D_actual)
+            signD = step_function(D_actual)
             
             if(signD != y[j]):
                 weight[0] = weight[0] + learning_rate * signD * 1
@@ -32,16 +32,86 @@ def train(X, y, epoch, learning_rate):
 # predict method
 def predict(model, X):
     output = model[0] + model[1]*X[0] + model[2]*X[1]
-    signD = activation(output)
+    signD = step_function(output)
     return signD
         
 
 # activation method
-def activation(x):
+def step_function(x):
     if(x >= 0):
         signD = -1
     else:
         signD = 1
-
     return signD
+
+
+# function for training the model on iris dataset
+def train_irismodel(X_train, y_train, epochs, learnrate, n_hidden):
+    # no. of rows, no. of columns
+    n_records, n_features = X_train.shape
+    last_loss = None
+    
+    # initializing the size of weight vectors
+    weights_input_hidden = np.random.normal(scale = 1 / n_features ** 0.5, size = (n_features, n_hidden))
+    weights_hidden_output = np.random.normal(scale = 1 / n_features ** 0.5, size = n_hidden)
+    
+    for e in range(epochs):
+        # initializing all the delta weights with zeros
+        del_w_input_hidden = np.zeros(weights_input_hidden.shape)
+        del_w_hidden_output = np.zeros(weights_hidden_output.shape)
+        # In each epoch, and in each value of column
+        for x, y in zip(X_train.values, y_train):
+            # forward pass
+            hidden_input = np.dot(x, weights_input_hidden)
+            hidden_output = sigmoid(hidden_input)
+            output = sigmoid(np.dot(hidden_output, weights_hidden_output))
+            
+            # backward pass
+            error = y - output
+            # calculate the error term for the output unit,
+            output_error_term = error * output * (1 - output)
+            
+            # hidden layer's contribution to the error
+            hidden_error = np.dot(output_error_term, weights_hidden_output)
+            # error term for the hidden layer
+            hidden_error_term = hidden_error * hidden_output * (1 - hidden_output)
+            
+            # update the change in weights
+            del_w_hidden_output += output_error_term * hidden_output
+            del_w_input_hidden += hidden_error_term * x[:, None]
+        
+        # updating weights
+        weights_input_hidden += learnrate * del_w_input_hidden / n_records
+        weights_hidden_output += learnrate * del_w_hidden_output / n_records 
+        
+        # Printing out the mean square error on the training set
+        if e % (epochs / 10) == 0:
+            print('epoch : ', e)
+            hidden_output = sigmoid(np.dot(x, weights_input_hidden))
+            out = sigmoid(np.dot(hidden_output, weights_hidden_output))
+            loss = np.mean((out - y_train) ** 2)
+            print("training accuracy: ", 1 - loss)
+
+    print('training complete')
+    
+    model = [weights_input_hidden, weights_hidden_output]
+    
+    return model
+    
+           
+# activation function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def predict_irismodel(X_test, y_test, model):
+    
+    hidden = sigmoid(np.dot(X_test, model[0]))
+    out = sigmoid(np.dot(hidden, model[1]))
+    # threshold
+    predictions = out > 0.5
+    accuracy = np.mean(predictions == y_test)
+    print("testing accuracy: ", accuracy)
+    return predictions
+    
 
