68e798f1175c9edc1811c041b86978c4966effde
Rajat Sharma <rajatsharma369007@gmail.com>
2019-04-04 12:46:07 +0530

    new laboratory assignment
    train a model to classify the iris dataset

--- assignment_3/utils/read_dataset.py
+++ assignment_3/utils/read_dataset.py
@@ -4,6 +4,7 @@ This script helps in reading the dataset
 # importing libraries
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
     
 # function for splitting data
 def read_data(pathname):
@@ -17,14 +18,30 @@ def read_data(pathname):
     return X, D_desired
     
 
-def read_iris(pathname):
-    # reading dataset
+def read_irisdata(pathname):
+    # reading csv data
     df = pd.read_csv(pathname)
-    # for 2 category
-    X_df = df[0:100][['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
-    X = np.array(X_df)
-    y_df = df[0:100]['Species']
-    y = np.array(pd.get_dummies(y_df, drop_first=True))
+
+    # Make dummy variables for rank
+    df = pd.concat([df, pd.get_dummies(df['Species'], drop_first=True)], axis=1)
+    df = df.drop(['Id','Species'], axis=1)
     
-    return X, y
+    # normalizing the values
+    scaler = MinMaxScaler()
+    normalized_data = scaler.fit_transform(df)
+    normalized_df = pd.DataFrame(normalized_data)
+    
+    # Splitting 10% of the data for testing
+    np.random.seed(21)
+    sample = np.random.choice(normalized_df.index, size=int(len(normalized_df) \
+                                                            *0.9), replace=False)
+    
+    # splitting rows
+    train_data, test_data = normalized_df.ix[sample], normalized_df.drop(sample)
+    
+    # splitting features and targets
+    X_train, y_train = train_data.drop(4, axis=1), train_data[4]
+    X_test, y_test = test_data.drop(4, axis=1), test_data[4]
+    
+    return X_train, X_test, y_train, y_test
     
